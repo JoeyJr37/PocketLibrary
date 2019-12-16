baebd38fd73a17b4367c00ba36d2e300a0d52462
JoeyJr37 <JoeyJr11@gmail.com>
2019-12-16 06:48:53 -0600

    Updated Read Status functionality

--- main.js
+++ main.js
@@ -1,105 +1,115 @@
+/* eslint-disable wrap-iife */
 "use strict";
+
 let myLibrary = [];
 let newBookBtn = document.querySelector("#addNewBook");
 
-function make(element){
-	return document.createElement(element);
-	};
+function make(element) {
+    return document.createElement(element);
+};
 
 // Book object constructor
 function Book(title, author, pages, readStatus, summary) {
-	this.author = author;
-	this.title = title;
-	this.pages = pages;
-	this.readStatus = false;
-	this.summary = summary;
-};
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.readStatus = readStatus;
+    this.summary = summary;
+}
 
-//function that adds books to library
+// function that adds books to library
 function addBookToLibrary(bookObj) {
-	let newBook = new Book(bookObj.title, bookObj.author, bookObj.pages, bookObj.readStatus, bookObj.summary);
-	myLibrary.push(newBook);
-	return myLibrary;
-	};
+    let newBook = new Book(bookObj.title, bookObj.author, bookObj.pages, bookObj.readStatus, bookObj.summary);
+    myLibrary.push(newBook);
+    return myLibrary;
+}
 
 function getBookInfo() {
-let title = document.querySelector("#book-title").value;
-let author = document.querySelector("#book-author").value;
-let pages = document.querySelector("#book-pages").value;
-let summary = document.querySelector("#book-summary").value;
-let readStatus = document.querySelector("#book-read-status");
-let bookInfo = {
-		title,
-		author,
-		pages,
-		summary,
-		readStatus};
-	addBookToLibrary(bookInfo);
-	};
+    let title = document.querySelector("#book-title").value;
+    let author = document.querySelector("#book-author").value;
+    let pages = document.querySelector("#book-pages").value;
+    let summary = document.querySelector("#book-summary").value;
+    let readStatus = document.querySelector("input[name = 'book-read-status']:checked").value;
+    let bookInfo = {
+        title,
+        author,
+        pages,
+        summary,
+        readStatus};
+    addBookToLibrary(bookInfo);
+}
 function closeModal() {
-  modal.style.display = "none";
-};
+    modal.style.display = "none";
+}
 
 function clearBookInput() {
-	document.querySelector("#book-title").value = "";
-	document.querySelector("#book-author").value = "";
-	document.querySelector("#book-pages").value = "";
-	document.querySelector("#book-summary").value = "";
-	};
+    document.querySelector("#book-title").value = "";
+    document.querySelector("#book-author").value = "";
+    document.querySelector("#book-pages").value = "";
+    document.querySelector("#book-summary").value = "";
+    document.getElementById("YES").checked = false;
+    document.getElementById("NO").checked = false;
+};
 
-function createNewCell(element, objPropertyValue){
-	let newCell = make(element);
-	newCell.textContent = objPropertyValue;
-	return newCell;
-	};
+function createNewCell(element, objPropertyValue) {
+    let newCell = make(element);
+    newCell.textContent = objPropertyValue;
+    return newCell;
+};
 
 Book.prototype.updateReadStatus = function() {
-	this.classList.toggle("read");
-	if (this.style.backgroundColor === "red") {this.textContent = "READ";}
-	else {this.textContent = "NOT READ";};
-		};
+    this.classList.toggle("read");
+    if (this.style.backgroundColor === "red") {this.textContent = "READ";}
+    else {this.textContent = "NOT READ";}
+};
+
+const setReadStatus = (bookObj) => {
+	let readStatusCell;
+	if (bookObj.readStatus === "YES") {
+		readStatusCell = createNewCell("td", "READ");
+    } else {
+		readStatusCell = createNewCell("td", "NOT READ");
+    };
+    return readStatusCell;
+};
 
 function render() {
-	getBookInfo();
-	document.querySelector(".book_table_body").remove();
-	let bookTable = make("tbody");
-	bookTable.setAttribute("class", "book_table_body");
-	document.querySelector(".book_table").append(bookTable);
-	myLibrary.forEach((book) => {
-		let bookTitle = createNewCell("td", book.title);
-		let bookAuthor = createNewCell("td", book.author);
-		let bookPages = createNewCell("td", book.pages);
-		let bookSummary = createNewCell("td", book.summary);
-		let readStatusCell = createNewCell("td", book.readStatus);
-		let readStatusBtn = make("button");
-		readStatusBtn.setAttribute("class", "readStatusBtn");
-		let deleteBtnCell = createNewCell("td");
-		readStatusBtn.textContent = "NOT READ";
-		readStatusCell.append(readStatusBtn);
-		let deleteBtn = make("button");
-		deleteBtn.textContent = "DELETE";
-		deleteBtnCell.append(deleteBtn);
-		let newRow = make("tr");
-		bookTable.append(newRow);
-		newRow.append(bookTitle);
-		bookTitle.after(bookAuthor);
-		bookAuthor.after(bookPages);
-		bookPages.after(bookSummary);
-		bookSummary.after(readStatusCell);
-		readStatusCell.after(deleteBtnCell);
-		let bookID = myLibrary.indexOf(book) + 1;
-		newRow.setAttribute("id", bookID);
-		let readStatusBtns = document.querySelectorAll(".readStatusBtn");
-		readStatusBtns.forEach(item => item.addEventListener("click", book.updateReadStatus));	
-		deleteBtn.addEventListener("click", () => {
-			let thisRow = document.getElementById(bookID);
-			thisRow.remove();
-			let idNumber = bookID - 1;
-			myLibrary.splice(idNumber, 1);});
-	});
-	clearBookInput();
-	closeModal();
-};
+    getBookInfo();
+    document.querySelector(".book_table_body").remove();
+    let bookTable = make("tbody");
+    bookTable.setAttribute("class", "book_table_body");
+    document.querySelector(".book_table").append(bookTable);
+    myLibrary.forEach((book) => {
+        let bookTitle = createNewCell("td", book.title);
+        let bookAuthor = createNewCell("td", book.author);
+        let bookPages = createNewCell("td", book.pages);
+        let bookSummary = createNewCell("td", book.summary);
+        let readStatusCell = setReadStatus(this);
+        let deleteBtnCell = createNewCell("td");
+        let deleteBtn = make("button");
+        deleteBtn.textContent = "DELETE";
+        deleteBtnCell.append(deleteBtn);
+        let newRow = make("tr");
+        bookTable.append(newRow);
+        newRow.append(bookTitle);
+        bookTitle.after(bookAuthor);
+        bookAuthor.after(bookPages);
+        bookPages.after(bookSummary);
+        bookSummary.after(readStatusCell);
+        readStatusCell.after(deleteBtnCell);
+        let bookID = myLibrary.indexOf(book) + 1;
+        newRow.setAttribute("id", bookID);
+        let readStatusBtns = document.querySelectorAll(".readStatusBtn");
+        readStatusBtns.forEach(item => item.addEventListener("click", book.updateReadStatus));	
+        deleteBtn.addEventListener("click", () => {
+            let thisRow = document.getElementById(bookID);
+            thisRow.remove();
+            let idNumber = bookID - 1;
+            myLibrary.splice(idNumber, 1);});
+    });
+    clearBookInput();
+    closeModal();
+}
 
 newBookBtn.addEventListener("click", render);
 // Get the modal
@@ -113,7 +123,7 @@ const span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 newBookbtn.onclick = function() {
-  modal.style.display = "block";
+    modal.style.display = "block";
 };
 
 // add button on each book's display to change it's read status
